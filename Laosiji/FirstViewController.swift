657b7b0252f8560fb3808c3d43a274408e0f0ee0
Luo Sheng <luosheng1986@gmail.com>
2016-06-04 07:40:26 +0800

    Initial Commit

---
+++ Laosiji/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Laosiji
+//
+//  Created by Luo Sheng on 16/6/4.
+//  Copyright © 2016年 Luo Sheng. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
