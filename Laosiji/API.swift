63ae3c2394fb46cd537f6912310cd732743bd5ea
Luo Sheng <luosheng1986@gmail.com>
2016-06-04 08:34:32 +0800

    Add API struct.

--- Laosiji/API.swift
+++ Laosiji/API.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import Alamofire
+
+struct API {
+    
+    enum Router: URLRequestConvertible {
+        static let baseURLString = "http://capi.douyucdn.cn/api/v1/"
+        
+        case GetColumnDetail(column: String)
+        
+        var URLRequest: NSMutableURLRequest {
+            let result: (path: String, parameters: [String: AnyObject]) = {
+                switch self {
+                case .GetColumnDetail(let column):
+                    return ("getColumnDetail", ["shortName": column])
+                }
+            }()
+            
+            let URL = NSURL(string: Router.baseURLString)!
+            let URLRequest = NSURLRequest(URL: URL.URLByAppendingPathComponent(result.path))
+            let encoding = Alamofire.ParameterEncoding.URL
+            
+            return encoding.encode(URLRequest, parameters: result.parameters).0
+        }
+    }
+}
