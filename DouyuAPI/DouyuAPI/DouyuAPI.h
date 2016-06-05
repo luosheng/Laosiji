a5441a72ccf77f75f5b59972e41ef99baef01303
Luo Sheng <luosheng1986@gmail.com>
2016-06-05 21:42:45 +0800

    Create DouyuAPI project combine it with the UI project.

---
+++ DouyuAPI/DouyuAPI/DouyuAPI.h
@@ -0,0 +1,19 @@
+//
+//  DouyuAPI.h
+//  DouyuAPI
+//
+//  Created by Luo Sheng on 16/6/5.
+//  Copyright © 2016年 Luo Sheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DouyuAPI.
+FOUNDATION_EXPORT double DouyuAPIVersionNumber;
+
+//! Project version string for DouyuAPI.
+FOUNDATION_EXPORT const unsigned char DouyuAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DouyuAPI/PublicHeader.h>
+
+
