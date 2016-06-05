5423134dc0b186d54f8c4efb87e4052b99ec054f
Luo Sheng <luosheng1986@gmail.com>
2016-06-06 07:06:23 +0800

    Add rooms view controller and view cell.

---
+++ Laosiji/RoomViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  RoomViewCell.swift
+//  Laosiji
+//
+//  Created by Luo Sheng on 16/6/6.
+//  Copyright © 2016年 Luo Sheng. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RoomViewCell: UICollectionViewCell {
+    
+}
---
+++ Laosiji/RoomsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RoomsViewController.swift
+//  Laosiji
+//
+//  Created by Luo Sheng on 16/6/6.
+//  Copyright © 2016年 Luo Sheng. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class RoomsViewController: UICollectionViewController {
+    
+}
