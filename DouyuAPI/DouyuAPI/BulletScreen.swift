5a9361130f82c9e9ffb0dd5b4e13be53d3800956
LUO <luosheng1986@gmail.com>
2016-06-07 13:30:15 +0800

    Add basic functions to get bullets from server.

--- DouyuAPI/DouyuAPI/BulletScreen.swift
+++ DouyuAPI/DouyuAPI/BulletScreen.swift
@@ -9,7 +9,13 @@
 import Foundation
 import CocoaAsyncSocket
 
-public class BulletScreen: NSObject, GCDAsyncSocketDelegate {
+public class BulletScreen: GCDAsyncSocketDelegate {
+    
+    enum Tag: Int {
+        case Login = 1
+        case Join
+        case General
+    }
     
     let roomID: String
     var socket: GCDAsyncSocket!
@@ -17,18 +23,58 @@ public class BulletScreen: NSObject, GCDAsyncSocketDelegate {
     public init(roomID: String) {
         self.roomID = roomID
         
-        super.init()
-        
         socket = GCDAsyncSocket(delegate: self, delegateQueue: dispatch_get_main_queue())
         try! socket.connectToHost("openbarrage.douyutv.com", onPort: 8601, withTimeout: 10)
     }
     
-    public func socket(sock: GCDAsyncSocket!, didConnectToHost host: String!, port: UInt16) {
+    @objc public func socket(sock: GCDAsyncSocket!, didConnectToHost host: String!, port: UInt16) {
+        let loginCommand = "type@=loginreq/roomid@=\(roomID)/"
+        sendCommandTo(sock, command: loginCommand, tag: .Login)
+    }
+    
+    @objc public func socketDidDisconnect(sock: GCDAsyncSocket!, withError err: NSError!) {
         
     }
     
-    public func socketDidDisconnect(sock: GCDAsyncSocket!, withError err: NSError!) {
+    @objc public func socket(sock: GCDAsyncSocket!, didReadData data: NSData!, withTag tag: Int) {
+        let subdata = data.subdataWithRange(NSRange(location: 12, length: data.length - 12))
+        let response = String(data: subdata, encoding: NSUTF8StringEncoding)
         
+        print(response)
+        
+        guard let t = Tag(rawValue: tag) else {
+            return
+        }
+        
+        switch t {
+        case .Login:
+            let joinCommand = "type@=joingroup/rid@=\(roomID)/gid@=-9999/"
+            sendCommandTo(sock, command: joinCommand, tag: .Join)
+        default:
+            break
+        }
+        
+        sock.readDataWithTimeout(-1, tag: Tag.General.rawValue)
+    }
+    
+    private func toBytes(str: String) -> NSData {
+        var bytes: [UInt8] = []
+        let content = Array(str.utf8)
+        let length: [UInt8] = [UInt8(content.count + 0x09), 0x00, 0x00, 0x00]
+        bytes += length
+        bytes += length
+        bytes += [0xb1, 0x02, 0x00, 0x00]
+        bytes += content
+        bytes += [0x0]
+        
+        let data = NSData(bytes: bytes)
+        return data
+    }
+    
+    private func sendCommandTo(sock: GCDAsyncSocket, command: String, tag: Tag) {
+        let data = toBytes(command)
+        sock.writeData(data, withTimeout: -1, tag: tag.rawValue)
+        sock.readDataWithTimeout(-1, tag: tag.rawValue)
     }
     
 }
