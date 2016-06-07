2853b3ac5b4596826089660fc55fb0e98a89831c
LUO <luosheng1986@gmail.com>
2016-06-07 13:58:12 +0800

    Add bullet message object.

---
+++ DouyuAPI/DouyuAPI/Models/BulletMessage.swift
@@ -0,0 +1,28 @@
+//
+//  BulletMessage.swift
+//  DouyuAPI
+//
+//  Created by Luo Sheng on 6/7/16.
+//  Copyright © 2016 Luo Sheng. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+public class BulletMessage: Mappable {
+    
+    public var text: String?
+    public var name: String?
+    public var level: String?
+    
+    required public init?(_ map: Map) {
+        
+    }
+    
+    public func mapping(map: Map) {
+        text <- map["txt"]
+        name <- map["nn"]
+        level <- map["level"]
+    }
+    
+}
